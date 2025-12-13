f1e8673d5a9b7cb47f5949f066cedacb1633cb00
owen-hwlee <hwolee@connect.ust.hk>
2025-12-13 02:22:23 -0800

    completed day05 part2

--- AdventOfCode2025/Day05.cs
+++ AdventOfCode2025/Day05.cs
@@ -2,10 +2,11 @@
 {
     internal static void Run(TextReader reader)
     {
-        List<(ulong Lower, ulong Upper)> ranges = reader.GetRanges().ToList();
-        List<ulong> ingredientIDs = reader.GetIngredientIDs().ToList();
-        int resultPart1 = ingredientIDs.FindFresh(ranges).Count();
+        List<(ulong Lower, ulong Upper)> ranges = reader.GetRanges().OrderBy(r => r.Lower).ToList();
+        int resultPart1 = reader.GetIngredientIDs().FindFresh(ranges).Count();
         Console.WriteLine($"Answer to Part 1 = {resultPart1}");
+        ulong resultPart2 = ranges.MergeOverlaps().GetNumOfFresh().Sum();
+        Console.WriteLine($"Answer to Part 2 = {resultPart2}");
     }
 
     private static IEnumerable<ulong> FindFresh(this IEnumerable<ulong> ids, IEnumerable<(ulong Lower, ulong Upper)> ranges) => ids
@@ -13,6 +14,27 @@
 
     private static bool IsWithin(this ulong ingredientID, (ulong Lower, ulong Upper) pair) =>
         pair.Lower <= ingredientID && pair.Upper >= ingredientID;
+
+    private static IEnumerable<(ulong Lower, ulong Upper)> MergeOverlaps(this IEnumerable<(ulong Lower, ulong Upper)> ranges)
+    {
+        (ulong lower, ulong upper) = ranges.First();
+        foreach ((ulong Lower, ulong Upper) in ranges.Skip(1))
+        {
+            if (upper >= Lower)
+            {
+                upper = Math.Max(upper, Upper);
+            }
+            else
+            {
+                yield return (lower, upper);
+                (lower, upper) = (Lower, Upper);
+            }
+        }
+        yield return (lower, upper);
+    }
+
+    private static IEnumerable<ulong> GetNumOfFresh(this IEnumerable<(ulong Lower, ulong Upper)> ranges) => ranges
+        .Select(r => r.Upper - r.Lower + 1);
 
     private static IEnumerable<(ulong Lower, ulong Upper)> GetRanges(this TextReader reader) => reader
         .ReadLines()
